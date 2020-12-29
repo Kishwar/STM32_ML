72a5d67d88cb51961a409b1eb8fa2fdf91f90f35
Kishwar Kumar <kumar.kishwar@gmail.com>
2020-12-29 20:19:53 +0100

    MNIST On STM32F446 Nucleo-64
    MNIST On STM32F446 Nucleo-64

---
+++ STM32_MNIST/src/main.cc
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    main.c [MNIST on STM32F446 Nucleo-64
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2020
+  * @brief   Default main function.
+  * @memory  512Kbytes Flash, 128KByte SRAM
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+			
+
+int main(void)
+{
+
+	for(;;);
+}
